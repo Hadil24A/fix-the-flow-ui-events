7cc9c83a4f4086e298556c9086b1849062829a1b
Hadil24A <hadil.ahmed@hva.nl>
2023-12-11 11:46:49 +0100

    design, flow, feedforward

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,37 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Element 1
+
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('click', function () {
+  design.classList.toggle('design-flip')
+})
+
+
+// Element 2
+
+let flow = document.querySelector('a:nth-of-type(8)')
+
+flow.addEventListener('mouseover', function () {
+  flow.classList.toggle('flow-rotate')
+})
+
+
+//  Element 3
+
+let feedforward = document.querySelector('a:nth-of-type(16)')
+
+feedforward.addEventListener('dblclick', function (){
+  feedforward.hidden = true
+})
+
+// Element 4
+
+let development = document.querySelector('a:nth-of-type(4)')
+
+development.addEventListener('keydown' function (){
+  
+})
